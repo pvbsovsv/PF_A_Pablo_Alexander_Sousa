988e6ebe8640cfdcc39f541a6e8312381a190924
pvbsovsv <pablo.a.sousa@gmail.com>
2025-11-09 19:19:21 +0000

    changes in base js

--- js/base.js
+++ js/base.js
@@ -6,6 +6,8 @@ gsap.registerPlugin(ScrollTrigger);
 
 let mm = gsap.matchMedia();
 
+weAreClosed();
+
 //media queries
 
 mm.add("(min-width: 601px)", () => {
@@ -407,6 +409,49 @@ function killAllTweens() {
   // Optionally reset all inline styles added by GSAP
   gsap.set("*", { clearProps: "all" });
 }
+
+// we are open we are closed 
+function weAreClosed() {
+  //data
+  const dateNow = new Date();
+  const optionsDate = {
+    weekday: "long",
+  };
+
+  //hora
+  const timeNow = {
+    hours: dateNow.getHours(),
+    minutes: dateNow.getMinutes(),
+    seconds: dateNow.getSeconds(),
+  };
+
+  let day = dateNow.getDay();
+  let hours = timeNow.hours;
+  let minutes = timeNow.minutes;
+  if (hours < 1) {
+    hours = "0" + hours;
+  }
+  if (minutes < 10) {
+    minutes = "0" + minutes;
+  }
+
+  const isWeekend = day === 0 || day === 6;
+
+  const target = document.getElementById("dateTime");
+
+  if (!isWeekend && hours >= 9 && hours <= 18) {
+    target.innerHTML = `${hours}:${minutes}  ${dateNow.toLocaleString(
+      "en-EN",
+      optionsDate
+    )}, we are open `;
+  } else {
+    target.innerHTML = `${hours}:${minutes}  ${dateNow.toLocaleString(
+      "en-EN",
+      optionsDate
+    )}, we are closed `;
+  }
+}
+
 
 
 
