1089a9056ec4f878b17258c1fccebc58956b39ae
pvbsovsv <pablo.a.sousa@gmail.com>
2025-11-13 19:56:41 +0000

    contact form send email changes

--- js/base.js
+++ js/base.js
@@ -453,6 +453,49 @@ function weAreClosed() {
 }
 
 
+//email contact form
+
+document.getElementById('contact-form').addEventListener('submit', async (e) => {
+  //fazemos com que a pagina nao refreque 
+  e.preventDefault();
+
+  //values do form 
+
+  const name = document.getElementById('name').value
+  const email = document.getElementById('email').value
+  const phone = document.getElementById('phone').value
+  const project = document.getElementById('project').value
+
+  //Status para mostrar ao cliente
+  const status = document.getElementById('status'); 
+  status.textContent = 'Sending Request..'; 
+
+  try {
+    
+      
+  const response = await fetch('https://wlx-backend-mailer.onrender.com/send-email', {
+    method: 'POST', 
+    headers: {'Content-Type': 'application/json'}, 
+    body: JSON.stringify({name, email, phone, project}),
+  }); 
+
+  const result = await response.json()
+
+
+  if (result.success){status.textContent = 'Message sent successfully!'}
+
+  else{status.textContent = 'Not able to send message.'}
+
+  } 
+  
+  catch (error) {
+    console.error('Error:', error);
+      status.textContent = 'Error sending message. Please try again later.';
+  }
+
+
+  
+})
 
 
 
